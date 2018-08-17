6aba6fa45591685b188ad1a5bef832e7e116c1ab
Zaparoli <luizzaparoli07@gmail.com>
2018-08-17 02:33:03 -0300

    E-mail Configurado

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Contatos;
 use Redirect;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class ContatosController extends Controller
 {
@@ -24,9 +25,13 @@ class ContatosController extends Controller
 
     public function salvar(Request $request){
 
- 		$contato = new Contatos();
+		$contato = new Contatos();
+		 
+		$contato = $contato->create($request->all());
 
-    	$contato = $contato->create($request->all());
+		$email = $request->input('email');
+
+		$this -> sendEmail($email);
 
     	\Session::flash('Sucesso', 'Cliente Cadastrado com Sucesso!');
 
@@ -64,5 +69,57 @@ class ContatosController extends Controller
 
  		return Redirect::to('contatos/');
  	 	
- 	}
+	}
+
+	public function sendEmail($email) {
+
+		$Mailer = new PHPMailer();
+
+		//SMTP
+		$Mailer -> IsSMTP();
+
+		//Configuração Carácteres
+		$Mailer -> Charset = 'UTF-8';
+
+		//Enviar E-mail em HTML
+		$Mailer -> isHTML(true);
+
+		//Configurações
+		$Mailer -> SMTPAuth = true;
+		$Mailer -> SMTPSecure = 'tls';
+
+		//Nome do Servidor
+		$Mailer -> Host = 'smtp.mailtrap.io';
+		$Mailer -> Username = 'fc4a0d260cdac2';
+		$Mailer -> Password = '882f6026837d56';
+		$Mailer -> Port = 465;
+
+		//E-mail Remetente
+		$Mailer -> From = $email;
+
+		//Nome do Remetente
+		$Mailer -> FromName = 'Luiz Zaparoli';
+
+		//Assunto da Mensagem
+		$Mailer -> Subject = 'Projeto Tray';
+
+		//Corpo da Mensagem
+		$Mailer -> Body = 'Sejá Bem-Vindo(a) à minha Lista de Contatos ;D';
+
+		//Corpo da Mensagem em Texto
+		$Mailer -> AltBody = 'Conteúdo do E-mail em Text';
+
+		//Destinatário
+		$Mailer -> addAddress('luizzaparoli07@gmail.com');
+
+		if($Mailer -> Send()){
+
+			echo "E-mail enviado com sucesso";
+
+		}else{
+
+			echo "Erro no envio" . $Mailer -> ErrorInfo;
+
+		}
+	}
 }
